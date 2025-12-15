28ce4a224246481b8d9c2da2c8293b26c8fc9c3c
Jan Huber <huber@ubique.ch>
2025-12-15 21:15:51 +0100

    Initial Commit

---
+++ MontainGenerator/MontainGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MontainGeneratorApp.swift
+//  MontainGenerator
+//
+//  Created by Jan Huber on 15.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MontainGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
